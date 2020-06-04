627a460ffd9c2ba09d82c3e9f5d6130b64eb45b1
zhangle <719552483@qq.com>
2020-06-04 12:02:52 +0800

    autoheight

--- src/main/resources/static/js/noticeHTMLmodel.js
+++ src/main/resources/static/js/noticeHTMLmodel.js
@@ -11,23 +11,11 @@ function drawEditor(){
   KindEditor.ready(function(K) {
   	//定义生成编辑器的文本类型
 	    	editor1 = K.create('textarea[name="content"]', {
-				cssPath : 'editor/plugins/code/prettify.css',
-				allowImageUpload: true, //上传图片框本地上传的功能，false为隐藏，默认为true--
-				allowImageRemote : false, //上传图片框网络图片的功能，false为隐藏，默认为true
-				formatUploadUrl:false,
-			    uploadJson : '/newsImgUpload',//文件上传请求后台路径
-			    afterUpload: function(url){this.sync();toastr.warning("a:"+url);}, //图片上传后，将上传内容同步到textarea中
-	            afterBlur: function(){this.sync();},   ////失去焦点时，将上传内容同步到textarea中
-              allowFileManager : true,
-					items: ['source', '|', 'fullscreen', 'undo', 'redo', 'print', 'cut', 'copy', 'paste',
-						'plainpaste', 'wordpaste', '|', 'justifyleft', 'justifycenter', 'justifyright',
-						'justifyfull', 'insertorderedlist', 'insertunorderedlist', 'indent', 'outdent', 'subscript',
-						'superscript', '|', 'selectall', '-',
-						'title', 'fontname', 'fontsize','forecolor','hilitecolor', '|', 'textcolor', 'bgcolor', 'bold',
-						'italic', 'underline', 'strikethrough', 'removeformat', '|', 'image',
-						'advtable', 'hr', 'emoticons', 'link', 'unlink', '|'
-					]
+	    		        minHeight : '800px',
+						cssPath : 'editor/plugins/code/prettify.css'
 	    	});
+	    	var autoheight=editor1.edit.doc.body.scrollHeight;//此处的editor为kindeditor
+	    	editor1.edit.setHeight(autoheight);
 	});
 }
 
@@ -66,8 +54,9 @@ function getNoticeInfo() {
 function stuffNotices(currentNoteInfo){
 	$(".noticeTitleArea").html(currentNoteInfo.tzbt);
 	KindEditor.html("#newsInfoBody", currentNoteInfo.tzzt);
-	var autoheight=editor1.edit.doc.body.scrollHeight;//此处的editor为kindeditor
-	editor1.edit.setHeight(autoheight);
+	$(".ke-edit-iframe");
+//	var autoheight=editor1.edit.doc.body.scrollHeight;//此处的editor为kindeditor
+//	editor1.edit.setHeight(autoheight);
 }
 
 
