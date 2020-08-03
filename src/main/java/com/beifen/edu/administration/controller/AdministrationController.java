051bbd03ba1befb4b48c749fc4aa49ea84cef923
mengyuanlearn <282450762@qq.com>
2020-08-03 18:42:09 +0800

    Revert "cccc"
    This reverts commit 9728230347ec40f026864323907e4c07368f6541.

--- src/main/java/com/beifen/edu/administration/controller/AdministrationController.java
+++ src/main/java/com/beifen/edu/administration/controller/AdministrationController.java
@@ -101,7 +101,7 @@ public class AdministrationController {
 		administrationPageService.newUser(edu990);
 
 		// 生成系统用户权限
-		//cccc
+		//aaaa
 		
 		Edu991 edu991 = new Edu991();
 		edu991.setJs(sysRole);
