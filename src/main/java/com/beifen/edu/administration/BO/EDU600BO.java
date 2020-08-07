de430af5a82a2dabb80e69460f024ddc21bbdf7d
719552483@qq.com <719552483@qq.com>
2020-08-07 10:59:26 +0800

    新增：新增BO层

---
+++ src/main/java/com/beifen/edu/administration/BO/EDU600BO.java
@@ -0,0 +1,9 @@
+package com.beifen.edu.administration.BO;
+
+import com.beifen.edu.administration.domian.Edu600;
+
+public class EDU600BO extends Edu600 {
+    private String proposerName;//申请人姓名
+    private String businessName;//业务类型名称
+    private String lastPersonName;//上一步审批人姓名
+}
