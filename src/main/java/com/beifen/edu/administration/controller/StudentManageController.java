0470cb159ba3a7b13012cd462afe041f8564bc21
mengyuanlearn <282450762@qq.com>
2020-09-01 17:44:25 +0800

    修改：开课计划修改

--- src/main/java/com/beifen/edu/administration/controller/StudentManageController.java
+++ src/main/java/com/beifen/edu/administration/controller/StudentManageController.java
@@ -311,7 +311,7 @@ public class StudentManageController {
      * @return
      */
 
-    @RequestMapping("/studentGetSchoolYear")
+    @RequestMapping("/getStudentXn")
     @ResponseBody
     public ResultVO studentGetSchoolYear(@RequestParam("userKey") String userKey) {
         ResultVO result =studentManageService.studentGetSchoolYear(userKey);
