7eb405af88b45b7c018744d061617d59dbb799b4
719552483@qq.com <719552483@qq.com>
2020-09-05 15:06:49 +0800

    修改：用户管理

--- src/main/resources/static/js/addUser.js
+++ src/main/resources/static/js/addUser.js
@@ -389,14 +389,14 @@ function stuffTable() {
 	function deparmentNamesFormatter(value, row, index) {
 		if(value===""||value==null||typeof value==="undefined"){
 			return [
-				'<div class="multipleInTableArea deparmentInTableArea'+row.bf990_ID+'"><span title="'+row.js+'" class="myTooltip deparmentTxt deparmentTxt' + row.bf990_ID + '">暂未绑定</span>' +
+				'<div class="multipleInTableArea deparmentInTableArea'+row.bf990_ID+'"><span title="'+row.deparmentNames+'" class="myTooltip deparmentTxt deparmentTxt' + row.bf990_ID + '">暂未绑定</span>' +
 				'<select class="myTableSelect mydeparmentTableSelect' +row.bf990_ID + '" name="userDeparment'+row.bf990_ID+'" id="userDeparment'+row.bf990_ID+'" multiple="true">' + departmentOptionStr + '' +
 				'</select></div>'
 			]
 				.join('');
 		}else{
 			return [
-				'<div class="multipleInTableArea deparmentInTableArea'+row.bf990_ID+'"><span title="'+row.js+'" class="myTooltip deparmentTxt deparmentTxt' + row.bf990_ID + '">' + row.deparmentNames + '</span>' +
+				'<div class="multipleInTableArea deparmentInTableArea'+row.bf990_ID+'"><span title="'+row.deparmentNames+'" class="myTooltip deparmentTxt deparmentTxt' + row.bf990_ID + '">' + row.deparmentNames + '</span>' +
 				'<select class="myTableSelect mydeparmentTableSelect' +row.bf990_ID + '" name="userDeparment'+row.bf990_ID+'" id="userDeparment'+row.bf990_ID+'" multiple="true">' + departmentOptionStr + '' +
 				'</select></div>'
 			]
