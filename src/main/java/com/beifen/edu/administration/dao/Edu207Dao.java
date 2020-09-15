986bcf8fb2951f4cd46681486ae2ac4361530d1f
mengyuanlearn <282450762@qq.com>
2020-09-15 16:28:26 +0800

    修改：教学任务书查询修改

--- src/main/java/com/beifen/edu/administration/dao/Edu207Dao.java
+++ src/main/java/com/beifen/edu/administration/dao/Edu207Dao.java
@@ -4,10 +4,17 @@ import com.beifen.edu.administration.domian.Edu207;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 
 @Configuration
 public interface Edu207Dao extends JpaRepository<Edu207, Long>, JpaSpecificationExecutor<Edu207> {
 
+    //根据排课ID删除关联
+    @Transactional
+    @Modifying
+    @Query(value = "delete from edu207 where Edu201_ID =?1", nativeQuery = true)
+    void deleteByscheduleId(String edu201Id);
 }
