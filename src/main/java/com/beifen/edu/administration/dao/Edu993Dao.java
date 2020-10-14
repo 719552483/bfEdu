003d2279ab04187e1451dcad583708a19eae6954
mengyuanlearn <282450762@qq.com>
2020-10-14 13:14:00 +0800

    新增：查找提醒事项

--- src/main/java/com/beifen/edu/administration/dao/Edu993Dao.java
+++ src/main/java/com/beifen/edu/administration/dao/Edu993Dao.java
@@ -13,16 +13,16 @@ import java.util.List;
 public interface Edu993Dao extends JpaRepository<Edu993, Long>, JpaSpecificationExecutor<Edu993> {
 
 	// 根据角色获取通知
-	@Query(value = "select * from edu993 e where e.department_code in ?1 and role_id = ?2", nativeQuery = true)
+	@Query(value = "select * from edu993 e where e.department_code in ?1 and e.role_id = ?2 and (e.notice_type = '01' or e.notice_type = '02')", nativeQuery = true)
 	public List<Edu993> getNoticesByRole(List<String> departments, String roleId);
 
 	// 根据关联人获取通知
-	@Query(value = "select * from edu993 e where e.user_id=?1 and role_id = ?2", nativeQuery = true)
+	@Query(value = "select * from edu993 e where e.user_id=?1 and e.role_id = ?2", nativeQuery = true)
 	public List<Edu993> getNoticesByUser(String userId,String roleId);
 
 	// 根据202id删除
 	@Transactional
 	@Modifying
-	@Query(value = "delete from edu993 e where e.business_id =?1 and notice_type = '03'", nativeQuery = true)
+	@Query(value = "delete from edu993 e where e.business_id =?1 and e.notice_type = '03'", nativeQuery = true)
 	void deleteByEdu202ID(String scheduleId);
 }
