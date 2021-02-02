0704b085aef503589f1da2143fb6a459180d85e0
mengyuanlearn <282450762@qq.com>
2021-02-02 13:05:51 +0800

    新增：调课权限修改

--- src/main/java/com/beifen/edu/administration/dao/Edu402Dao.java
+++ src/main/java/com/beifen/edu/administration/dao/Edu402Dao.java
@@ -1,12 +1,18 @@
 package com.beifen.edu.administration.dao;
 
+import com.beifen.edu.administration.domian.Edu400;
 import com.beifen.edu.administration.domian.Edu402;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 
 @Configuration
 public interface Edu402Dao extends JpaRepository<Edu402, Long>, JpaSpecificationExecutor<Edu402> {
 
+    @Query(value = "SELECT JSID FROM EDU402 where jsmc = '干事'",nativeQuery = true)
+    String findjsId();
 }
