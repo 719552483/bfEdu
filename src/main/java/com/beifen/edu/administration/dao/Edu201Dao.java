f0a11d8349167e6c281f7382f9daa6ce9d62d26b
719552483@qq.com <719552483@qq.com>
2021-02-02 10:06:54 +0800

    修改：后台代码提交

--- src/main/java/com/beifen/edu/administration/dao/Edu201Dao.java
+++ src/main/java/com/beifen/edu/administration/dao/Edu201Dao.java
@@ -91,6 +91,9 @@ public interface Edu201Dao extends JpaRepository<Edu201, Long>, JpaSpecification
 	@Query(value = "select e.* from Edu201 e where e.edu108_ID in ?1 and e.sfypk is null  and e.sszt = 'pass'",nativeQuery = true)
 	List<Edu201> queryCulturePlanIds(List<Long> current108s);
 
+	//根据108ID获取任务书集合
+	@Query(value = "select e.ls from Edu201 e where e.edu108_ID in ?1 and e.sfypk = 'T'  and e.sszt = 'pass' GROUP BY ls",nativeQuery = true)
+	List<String> queryCoursePlanIds(List<Long> current108s);
 
 	//根据权限获已发布的教学任务书
 	@Query(value = "select a.* from edu201 a,edu108 b,edu107 c " +
--- src/main/java/com/beifen/edu/administration/dao/TeachingScheduleViewDao.java
+++ src/main/java/com/beifen/edu/administration/dao/TeachingScheduleViewDao.java
@@ -16,5 +16,9 @@ public interface TeachingScheduleViewDao extends JpaRepository<SchoolTimetablePO
     @Query(value = "select distinct e.edu201_id from TEACHING_SCHEDULE_VIEW e where e.edu101_id = ?1 and e.xnid = ?2",nativeQuery = true)
     List<String> findEdu201IdsByEdu101Id(String edu101id, String semester);
 
+    //教师查询周课表New
+    @Query(value = "select e.* from TEACHING_SCHEDULE_VIEW e where e.edu101_id in ?1 and e.week = ?2 and e.xnid = ?3",nativeQuery = true)
+    List<SchoolTimetablePO> findAllByEdu101IdNew(List<String> edu101id, String weekTime, String semester);
+
 }
 
