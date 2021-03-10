a442210b67645a2e46b74a66124e5d76be84a003
719552483@qq.com <719552483@qq.com>
2021-03-10 17:51:08 +0800

    修改：问卷获取答案完成

--- src/main/resources/static/js/questionnaire.js
+++ src/main/resources/static/js/questionnaire.js
@@ -178,31 +178,9 @@ function beginAnswer(row){
 			hideloding();
 			if (backjson.code==200) {
 				$('#doQuestionModal').MultiStep({
-					data:[{
-						content:'Hi!!',
-						label:'1',
-						skip:true
-					},{
-						content:'This is a multi-step modal',
-						skip:true
-					},{
-						content:`You can have skip options`,
-						skip:true
-					},{
-						content:`<small>You can include html content as well!</small><br><br>
-                <div class="form-group">
-                    <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
-                  </div>
-                `,
-						skip:true
-					},{
-						content:`This is the end<br>Hold your breath and count to ten`,
-						skip:true
-					}],
+					data:reMarkSingleQuestion(backjson.data.allQuestions),
 					final:'确认提交问卷？',
-					title:'问卷',
+					title:backjson.data.title,
 					finalLabel:'提交',
 					prevText:'上一个',
 					nextText:'下一个',
@@ -210,9 +188,11 @@ function beginAnswer(row){
 					finishText:'确认提交',
 					modalSize:'lg'
 				});
+
 				$.showModal("#doQuestionModal",true);
 				$("#doQuestionModal").find(".modal-header").find(".close").find("span").html('');
 				$("#doQuestionModal").addClass("finalCheckCssArea");
+				toolTipUp(".tooltipCite");
 
 				//提示框取消按钮
 				$("#doQuestionModal").find(".modal-header").find(".close").unbind('click');
@@ -220,11 +200,187 @@ function beginAnswer(row){
 					$.hideModal();
 					e.stopPropagation();
 				});
+
 			} else {
 				toastr.warning(backjson.msg);
 			}
 		}
 	});
+}
+
+//填充问卷的问题
+function reMarkSingleQuestion(allQuestions){
+	var returnArray=new Array();
+	for (var i = 0; i < allQuestions.length; i++) {
+		var questionObject=new Object();
+		if(allQuestions[i].type==="radio"){
+			questionObject=drawRadio(allQuestions[i],i);
+		}else if(allQuestions[i].type==="check"){
+			questionObject=drawCheck(allQuestions[i],i);
+		}else if(allQuestions[i].type==="rate"){
+			questionObject=drawRate(allQuestions[i],i);
+		}else if(allQuestions[i].type==="answer"){
+			questionObject=drawAnswer(allQuestions[i],i);
+		}
+		returnArray.push(questionObject);
+	}
+	return returnArray;
+}
+
+//渲染单选题
+function drawRadio(info,index){
+	var returnObject=new Object();
+	var str='<div class="singleA_Q singleA_Q'+info.edu802_ID+'" type="'+info.type+'">'+
+		'<span class="questionTitle"><p class="questionTxt">'+info.title+'</p></span>';
+	for (var i = 0; i < info.ckeckOrRaidoInfo.length; i++) {
+		var singleInfo=info.ckeckOrRaidoInfo[i];
+		str+='<div class="col4 giveBottom overArea">'
+			+'<input type="radio" name="Q'+singleInfo.edu802_ID+'" class="blue noneOutline" value="'+singleInfo.edu803_ID+'"/><cite class="tooltipCite" title="'+singleInfo.checkOrRadioText+'">'+singleInfo.checkOrRadioText
+			+ '</cite></div>';
+	}
+	str+='</div>';
+
+	returnObject.content=str;
+	returnObject.label=(index+1).toString();
+	returnObject.skip=true;
+	return returnObject;
+}
+
+//渲染多选题
+function drawCheck(info,index){
+	var returnObject=new Object();
+	var str='<div class="singleA_Q singleA_Q'+info.edu802_ID+'" type="'+info.type+'">'+
+		'<span class="questionTitle"><p class="questionTxt">'+info.title+'</p></span>';
+	for (var i = 0; i < info.ckeckOrRaidoInfo.length; i++) {
+		var singleInfo=info.ckeckOrRaidoInfo[i];
+		str+='<div class="col4 giveBottom overArea">'
+			+'<input type="checkbox" name="Q'+singleInfo.edu802_ID+'" class="blue noneOutline" value="'+singleInfo.edu803_ID+'"/><cite class="tooltipCite" title="'+singleInfo.checkOrRadioText+'">'+singleInfo.checkOrRadioText
+			+ '</cite></div>';
+	}
+	str+='</div>';
+
+	returnObject.content=str;
+	returnObject.label=(index+1).toString();
+	returnObject.skip=true;
+	return returnObject;
+}
+
+//渲染评分题
+function drawRate(info,index){
+	var returnObject=new Object();
+	var str='<div class="singleA_Q singleA_Q'+info.edu802_ID+'" type="'+info.type+'">' +
+		'<fieldset class="starability-slot starability-growRotate">' +
+		'<span class="questionTitle"><p class="questionTxt">'+info.title+'</p></span>';
+	for (var i = 5; i > 0; i--) {
+		str+='<input type="radio" id="rate'+info.edu802_ID+i+'" name="Q'+info.edu802_ID+'" value="'+i+'" />'
+			+'<label for="rate'+info.edu802_ID+i+'" title="'+i+'分" class="tooltipCite">'+i+'分</label>';
+	}
+	str+='</fieldset></div>';
+
+	returnObject.content=str;
+	returnObject.label=(index+1).toString();
+	returnObject.skip=true;
+	return returnObject;
+}
+
+//渲染简答题
+function drawAnswer(info,index){
+	var returnObject=new Object();
+	var str='<div class="singleA_Q singleA_Q'+info.edu802_ID+'" type="'+info.type+'">'+
+		'<span class="questionTitle">' +
+		'<p class="questionTxt">'+info.title+'</p>' +
+		'</span>'
+		+'<textarea style="width: 480px;max-width: 500px;margin-top: -10px;margin-left:0px " maxlength="300"  type="text" class="breakOptionTextArea" id="Q'+info.edu802_ID+'" placeholder="请回答(最多300个字符,中文2个，英文1个)..." />'
+		+'</div>';
+
+	returnObject.content=str;
+	returnObject.label=(index+1).toString();
+	returnObject.skip=true;
+	return returnObject;
+}
+
+//获取答题结果并关闭模态框
+function getQuestionRs(){
+	var returnArray=new Array();
+	var allArea=$("#doQuestionModal").find(".singleA_Q");
+	for (var i = 0; i < allArea.length; i++) {
+		var returnObject=new Object();
+		var type=allArea[i].attributes[1].nodeValue;
+		var currentId=allArea[i].classList[1].replace("singleA_Q","");
+		if(type==="radio"||type==="rate"){
+			returnObject.questionId=currentId;
+			typeof $('input:radio[name="Q'+currentId+'"]:checked').val()==="undefined"?returnObject.answer='':returnObject.answer=$('input:radio[name="Q'+currentId+'"]:checked').val();
+		}else if(type==="check"){
+			var checkBoxVlue=new Array();
+			returnObject.questionId=currentId;
+			$('input:checkbox[name="Q'+currentId+'"]:checked').each(function() {
+				checkBoxVlue.push($(this).val());
+			});
+			returnObject.answer=checkBoxVlue;
+		}else if(type==="answer"){
+			returnObject.questionId=currentId;
+			returnObject.answer=$("#Q"+currentId).val();
+		}
+		returnArray.push(returnObject);
+	}
+	checkAnswer(returnArray);
+}
+
+//检查答案信息
+function checkAnswer(answers){
+	var noAnswerNum=0;
+	for (var i = 0; i <answers.length; i++) {
+		if(typeof answers[i].answer==="string"){
+			if(answers[i].answer===""){
+				noAnswerNum++;
+			}
+		}else{
+			if(answers[i].answer.length==0){
+				noAnswerNum++;
+			}
+		}
+	}
+
+	if(noAnswerNum==answers.length){
+		toastr.warning('暂未答题');
+		return;
+	}
+
+	sendAnswer(answers)
+}
+
+//发送答案信息
+function sendAnswer(answers){
+	// $.ajax({
+	// 	method : 'get',
+	// 	cache : false,
+	// 	url : "/",
+	// 	data: {
+	// 		"userId":$(parent.frames["topFrame"].document).find(".userName")[0].attributes[0].nodeValue,
+	// 		"SearchCriteria":JSON.stringify(serachObject)
+	// 	},
+	// 	dataType : 'json',
+	// 	beforeSend: function(xhr) {
+	// 		requestErrorbeforeSend();
+	// 	},
+	// 	error: function(textStatus) {
+	// 		requestError();
+	// 	},
+	// 	complete: function(xhr, status) {
+	// 		requestComplete();
+	// 	},
+	// 	success : function(backjson) {
+	// 		hideloding();
+	// 		if (backjson.code === 200) {
+	// 			toastr.info(backjson.msg);
+	// 			stuffAdministrationClassTable(backjson.data);
+	// 		} else {
+	// 			drawAdministrationClassEmptyTable();
+	// 			toastr.warning(backjson.msg);
+	// 		}
+	// 	}
+	// });
+	$.hideModal();
 }
 
 //页面按钮事件绑定
