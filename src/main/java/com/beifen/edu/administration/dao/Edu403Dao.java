542a305fb4b90716fbd74748c89dea9295b2e609
Xcccccc <819172455@qq.com>
2021-06-11 16:18:12 +0800

    修改展示已结课内容

--- src/main/java/com/beifen/edu/administration/dao/Edu403Dao.java
+++ src/main/java/com/beifen/edu/administration/dao/Edu403Dao.java
@@ -6,10 +6,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 
 @Configuration
 public interface Edu403Dao extends JpaRepository<Edu403, Long>, JpaSpecificationExecutor<Edu403> {
 
     @Query(value = "SELECT * FROM EDU403 ORDER BY xnid,ksz",nativeQuery = true)
-    Edu403 selectAll();
+    List<Edu403> selectAll();
 }
