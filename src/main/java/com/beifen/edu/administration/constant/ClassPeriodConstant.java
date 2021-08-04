0906638b7dc998f96d0bacbbc51c7c77c10445cf
719552483@qq.com <719552483@qq.com>
2021-08-04 17:01:25 +0800

    修改：学生课表

--- src/main/java/com/beifen/edu/administration/constant/ClassPeriodConstant.java
+++ src/main/java/com/beifen/edu/administration/constant/ClassPeriodConstant.java
@@ -3,11 +3,11 @@ package com.beifen.edu.administration.constant;
 //课节常量类
 public class ClassPeriodConstant {
 
-    public static String SECTION_ONE = "第一节";
-    public static String SECTION_TWO = "第二节";
-    public static String SECTION_THREE = "第三节";
-    public static String SECTION_FOUR = "第四节";
-    public static String SECTION_FIVE = "第五节";
-    public static String SECTION_SIX = "第六节";
+    public static String SECTION_ONE = "第1-2节";
+    public static String SECTION_TWO = "第3-4节";
+    public static String SECTION_THREE = "第5-6节";
+    public static String SECTION_FOUR = "第7-8节";
+    public static String SECTION_FIVE = "第9-10节";
+    public static String SECTION_SIX = "第11-12节";
 
 }
