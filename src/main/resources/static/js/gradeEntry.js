3c9e90966fbe9bfebbd8a492cc4bb1b7e1b2fa70
719552483@qq.com <719552483@qq.com>
2021-08-10 15:00:36 +0800

    修改：补考申请审批2

--- src/main/resources/static/js/gradeEntry.js
+++ src/main/resources/static/js/gradeEntry.js
@@ -3688,8 +3688,10 @@ function comfirmModifyReExamInfo(currentHistory,row){
 	//发送修改请求
 	$('.confirmRemindModify').unbind('click');
 	$('.confirmRemindModify').bind('click', function(e) {
+		var getApprovalobect=getApprovalobect();
+		getApprovalobect.businessType="10";
 		var SearchCriteria=new Object();
-		SearchCriteria.approvalInfo=getApprovalobect();
+		SearchCriteria.approvalInfo=getApprovalobect;
 		SearchCriteria.edu0051=modifyInfo;
 		snedModifyReExamInfo(SearchCriteria);
 		e.stopPropagation();
