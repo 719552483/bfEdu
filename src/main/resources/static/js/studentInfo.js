5ba1be6bb1bea0dda18679de1e079ef939aae6e4
719552483@qq.com <719552483@qq.com>
2021-11-24 14:25:01 +0800

    修改：批量毕业

--- src/main/resources/static/js/studentInfo.js
+++ src/main/resources/static/js/studentInfo.js
@@ -1337,13 +1337,11 @@ function modifyStudents(){
 
 //预备批量发放毕业证
 function graduationStudents(){
-
 	var reObject = new Object();
 	reObject.fristSelectId = "#graduationStudents_level";
 	reObject.actionSelectIds = "#graduationStudents_department,#graduationStudents_grade,#graduationStudents_major";
 	reReloadSearchsWithSelect(reObject);
 	stuffGraduationStudentsClassTable({});
-
 
 	$.showModal("#graduationStudentsModal",true);
 	LinkageSelectPublic("#graduationStudents_level","#graduationStudents_department","#graduationStudents_grade","#graduationStudents_major");
